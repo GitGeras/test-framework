58c2f898e6b424cfc55a2e1d98b25d1ee155fc16
Oleg <ovgerasin@gmail.com>
2018-09-04 17:38:18 +0300

    add csv and get request

--- src/main/java/com/db/gerasin/testframework/TestFrameworkApplication.java
+++ src/main/java/com/db/gerasin/testframework/TestFrameworkApplication.java
@@ -3,6 +3,8 @@ package com.db.gerasin.testframework;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -11,5 +13,10 @@ public class TestFrameworkApplication {
     @SneakyThrows
     public static void main(String[] args) {
         SpringApplication.run(TestFrameworkApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
---
+++ src/main/java/com/db/gerasin/testframework/service/CsvParser.java
@@ -0,0 +1,42 @@
+package com.db.gerasin.testframework.service;
+
+import com.db.gerasin.testframework.TestFrameworkApplication;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CsvParser {
+
+    /*@SneakyThrows
+    public static void main(String[] args) {
+        File file = new File(getPathName());
+        List<Map<String, String>> readList = read(file);
+        System.out.println(readList);
+    }*/
+
+    protected static String getPathName() {
+        return TestFrameworkApplication.class.getClassLoader().getResource(".").getFile() + "/expected.csv";
+    }
+
+    @SneakyThrows
+    public List<Map<String, String>> readFromFile() {
+        List<Map<String, String>> response = new LinkedList<>();
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+        MappingIterator<Map<String, String>> iterator = mapper.readerFor(Map.class)
+                .with(schema)
+                .readValues(new File(getPathName()));
+        while (iterator.hasNext()) {
+            response.add(iterator.next());
+        }
+        return response;
+    }
+}
